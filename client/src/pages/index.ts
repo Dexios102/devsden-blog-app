4ca397ceffb93aa668420d3a3c0cf7291b826173
Dexter Dave Cajayon <119906753+Dexios102@users.noreply.github.com>
2024-01-25 20:45:23 +0800

    client - created pages

---
+++ client/src/pages/index.ts
@@ -0,0 +1,8 @@
+import Login from "./Login";
+import Register from "./Register";
+import Home from "./Home";
+import Projects from "./Projects";
+import Dashboard from "./Dashboard";
+import About from "./About";
+
+export default { Login, Register, Home, Projects, Dashboard, About };
