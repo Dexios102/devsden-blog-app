4dafbd319be67fb0811f3558c80b746b0905d551
Dexter Dave Cajayon <119906753+Dexios102@users.noreply.github.com>
2024-01-26 13:19:59 +0800

    minor

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import devlogo from "@/assets/dev.svg";
 import ModeToggle from "./theme/mode-toggle";
 import me from "../assets/me.jpg";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { Link } from "react-router-dom";
 import { CgLogIn } from "react-icons/cg";
 import { CiSearch } from "react-icons/ci";
@@ -13,15 +14,15 @@ const Navbar = () => {
   return (
     <nav className="flex justify-between items-center">
       <Link to="/">
-        <button className="flex items-center gap-2">
+        <button className="flex items-center gap-2 w-full">
           <img src={devlogo} alt="dev_logo" className="w-10" />
           <h1 className="text-2xl font-bold gradient-text">DevsDen</h1>
         </button>
       </Link>
       <div
-        className="flex gap-6 font-medium 
-        bg-gray-100
-       rounded-full px-2  dark:bg-gray-900"
+        className="md:flex gap-6 font-medium 
+        bg-gray-100 mx-2
+       rounded-full px-2 dark:bg-gray-900 hidden"
       >
         <Link to="/">
           <Button variant="noneLine">Home</Button>
@@ -33,8 +34,8 @@ const Navbar = () => {
           <Button variant="noneLine">Projects</Button>
         </Link>
       </div>
-      <div className="flex items-center gap-4">
-        <div className="w-full max-w-sm relative">
+      <div className="flex items-center md:gap-4">
+        <div className="w-full max-w-sm relative hidden md:block">
           <Input placeholder="Search" type="text" />
           <CiSearch
             className="w-5 h-5 absolute right-3 top-1/2
@@ -45,18 +46,29 @@ const Navbar = () => {
           <ModeToggle />
         </div>
         {loggedIn ? (
-          <Avatar>
-            <AvatarImage src={me} />
-            <AvatarFallback>DD</AvatarFallback>
-          </Avatar>
+          <div className="hidden md:block">
+            <Link to="/profile">
+              <Avatar>
+                <AvatarImage src={me} />
+                <AvatarFallback>DD</AvatarFallback>
+              </Avatar>
+            </Link>
+          </div>
         ) : (
-          <div className="flex gap-4 items-center">
-            <Button>
-              <CgLogIn className="w-5 h-5 mr-2" />
-              Login
-            </Button>
+          <div className="md:flex gap-4 items-center hidden ">
+            <Link to="/login">
+              <Button>
+                <CgLogIn className="w-5 h-5 mr-2" />
+                Login
+              </Button>
+            </Link>
           </div>
         )}
+        <div className="md:hidden">
+          <Button variant="ghost">
+            <GiHamburgerMenu className="w-5 h-5" />
+          </Button>
+        </div>
       </div>
     </nav>
   );
