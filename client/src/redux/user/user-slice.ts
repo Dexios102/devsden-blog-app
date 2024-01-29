da39357a000a87b414c240fe6642260203d57c4a
Dexter Dave Cajayon <119906753+Dexios102@users.noreply.github.com>
2024-01-29 13:54:34 +0800

    user slice

---
+++ client/src/redux/user/user-slice.ts
@@ -0,0 +1,46 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface User {
+  username: string;
+  email: string;
+}
+
+interface UserState {
+  user: User | null;
+  loading: boolean;
+  error: string | null;
+}
+
+const initialState: UserState = {
+  user: null,
+  loading: false,
+  error: null,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    loginStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    loginSuccess: (state, action: PayloadAction<User>) => {
+      state.loading = false;
+      state.user = action.payload;
+    },
+    loginFailure: (state, action: PayloadAction<string>) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    logout: (state) => {
+      state.user = null;
+      state.loading = false;
+      state.error = null;
+    },
+  },
+});
+
+export const { loginStart, loginSuccess, loginFailure, logout } =
+  userSlice.actions;
+export default userSlice.reducer;
