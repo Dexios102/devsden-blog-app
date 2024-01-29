ee939203bf312123d4466c2a35022aeadb43ad33
Dexter Dave Cajayon <119906753+Dexios102@users.noreply.github.com>
2024-01-29 10:41:33 +0800

    addded react-redux and set provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,11 +7,15 @@ import { BrowserRouter as Router } from "react-router-dom";
 import axios from "axios";
 axios.defaults.baseURL = "http://localhost:5000/api/beta";
 axios.defaults.withCredentials = true;
+import { store } from "./redux/store.ts";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
